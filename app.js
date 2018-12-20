770dd562c5eb2fcac20879816fd15222623e0b30
zakkuri-manabu <zakkuri.manabu@gmail.com>
2018-12-20 13:06:46 +0900

    until lesson7

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
+app.use(helmet());
+app.use(compression()); //Compress all routes
+
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1:27017';
+var mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
